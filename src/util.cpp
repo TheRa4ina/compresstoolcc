c968450d30ec218803376b30fa4fa6d63678eb68
Павел Михайлов <pasha8165@gmail.com>
2024-10-28 00:49:51 +0300

    [fixup] correct array type and ch type

--- src/util.cpp
+++ src/util.cpp
@@ -3,14 +3,14 @@
 static constexpr size_t CHUNK_SIZE = 1024;
 CharFreqMap getFrequency(std::istream& is)
 {
-	CharFreqMap freq;
+	CharFreqMap freq{};
 	char buffer[CHUNK_SIZE] = { 0 };
-	char ch;
+	uint8_t ch;
 	while (is.read(buffer, CHUNK_SIZE) || is.gcount() > 0) {
 		std::size_t bytesRead = is.gcount();
 		for (size_t i = 0; i < bytesRead; i++)
 		{
-			ch = buffer[i];
+			ch = reinterpret_cast<uint8_t&>(buffer[i]);
 			++freq[ch];
 		}
 	}
